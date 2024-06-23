a0495abf9d2e182c1b918520f06b034d0619f11a
Rinz59 <danielmauro259@gmail.com>
2024-06-23 01:17:01 -0500

    Delete RINZ 2 ORIGINAL directory

--- RINZ 2 ORIGINAL/EVENTO DE PAGINADO.js
+++
@@ -1,8 +0,0 @@
-// Modifica el manejador de eventos de clic para los enlaces de paginación
-pageLink.addEventListener('click', (event) => {
-    currentPage = i;
-    renderGallery();
-    renderPagination();
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-  });
-  
--- RINZ 2 ORIGINAL/NAVEGACION ANIME.js
+++
@@ -1,4 +0,0 @@
-function navigateToAnime(title, image, description, episodes) {
-    const url = `REPETICION.html?title=${encodeURIComponent(title)}&image=${encodeURIComponent(image)}&description=${encodeURIComponent(description)}&episodes=${encodeURIComponent(JSON.stringify(episodes))}`;
-    window.location.href = url;
-}
